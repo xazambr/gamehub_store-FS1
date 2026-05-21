5e9c4df2f28902e742a4e5fca006953aa47b850d
xavier <xa.zambrano@duocuc.cl>
2026-05-21 18:29:31 -0400

    configurado POMs

---
+++ msvc-auth/src/main/java/com/duoc/msvc_auth/MsvcAuthApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.msvc_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAuthApplication.class, args);
+	}
+
+}
