d648f2a4613e5fdca7f31294f9e855b62a4237c2
xavier <xa.zambrano@duocuc.cl>
2026-05-21 16:05:01 -0400

    agregado microservicio auth

---
+++ mcsv-auth/src/main/java/com/duoc/mcsv_auth/McsvAuthApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.mcsv_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvAuthApplication.class, args);
+	}
+
+}
