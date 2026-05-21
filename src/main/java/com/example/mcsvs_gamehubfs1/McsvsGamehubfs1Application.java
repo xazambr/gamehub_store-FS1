2fb51dad309c07a568274c3b516ac08846437445
xazambr <xa.zambrano@duocuc.cl>
2026-05-21 12:12:06 -0400

    agregada base del projecto

---
+++ src/main/java/com/example/mcsvs_gamehubfs1/McsvsGamehubfs1Application.java
@@ -0,0 +1,13 @@
+package com.example.mcsvs_gamehubfs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvsGamehubfs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvsGamehubfs1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mcsvs_gamehubfs1/McsvsGamehubfs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcsvs_gamehubfs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvsGamehubfs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
