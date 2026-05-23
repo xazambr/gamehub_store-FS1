1fbf3ee4b8e31cd9a59e9ecf7646f4361e86224c
xavier <xa.zambrano@duocuc.cl>
2026-05-22 20:22:38 -0400

    agregado UserRepository

---
+++ msvc-users/src/main/java/com/duoc/msvc_users/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.duoc.msvc_users.repositories;
+
+import com.duoc.msvc_users.models.Usuario;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
