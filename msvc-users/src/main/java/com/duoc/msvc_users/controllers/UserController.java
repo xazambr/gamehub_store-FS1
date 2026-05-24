c3edae00b08f1747befe2195e2ee979042447916
xavier <xa.zambrano@duocuc.cl>
2026-05-23 22:44:39 -0400

    corregido enlace RequestMapping

--- msvc-users/src/main/java/com/duoc/msvc_users/controllers/UserController.java
+++ msvc-users/src/main/java/com/duoc/msvc_users/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/usuarios")
+@RequestMapping("/api/v1/usuarios")
 @Validated
 public class UserController {
     @Autowired
