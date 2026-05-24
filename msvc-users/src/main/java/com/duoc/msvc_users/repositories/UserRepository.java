571ab7e69000b377e0f3664ed4b1abc23cb0fb5a
xavier <xa.zambrano@duocuc.cl>
2026-05-23 22:44:57 -0400

    Eliminado import no usado

--- msvc-users/src/main/java/com/duoc/msvc_users/repositories/UserRepository.java
+++ msvc-users/src/main/java/com/duoc/msvc_users/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.duoc.msvc_users.repositories;
 
 import com.duoc.msvc_users.models.Usuario;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
