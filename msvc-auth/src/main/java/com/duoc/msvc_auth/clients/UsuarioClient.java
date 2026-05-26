1f4aa000dec4942e4b434cf5a7bf5c492d6630b8
xavier <xa.zambrano@duocuc.cl>
2026-05-26 01:43:41 -0400

    agregado findByEmail en usuarioClient

--- msvc-auth/src/main/java/com/duoc/msvc_auth/clients/UsuarioClient.java
+++ msvc-auth/src/main/java/com/duoc/msvc_auth/clients/UsuarioClient.java
@@ -4,9 +4,13 @@ import com.duoc.msvc_auth.models.dtos.ExtUsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="msvc-users", url = "localhost:8081/api/v1/usuarios")
 public interface UsuarioClient {
     @GetMapping("/{id}")
     ExtUsuarioDTO findById(@PathVariable Long id);
+
+    @GetMapping
+    ExtUsuarioDTO findByEmail(@RequestParam String email);
 }
